e27b293d26cfc5983246034ea6a2c17ad380b161
mingyouzhu <412102100@qq.com>
2023-12-14 11:37:33 +0800

    U

--- src/Acme.BookStore.Domain.Shared/ResponsePage.cs
+++ src/Acme.BookStore.Domain.Shared/ResponsePage.cs
@@ -9,23 +9,23 @@ namespace Acme.BookStore;
 /// <typeparam name="T"></typeparam>
 public class ResponsePage<T>
 {
-    public IList<T> Collection { get; private set; }
+    public IList<T> Collection { get; set; }
     /// <summary>
     /// 页号
     /// </summary>
-    public int PageNo { get; private set; }
+    public int PageNo { get; set; }
     /// <summary>
     /// 页大小
     /// </summary>
-    public int PageSize { get; private set; }
+    public int PageSize { get; set; }
     /// <summary>
     /// 总页
     /// </summary>
-    public int TotalPage { get; private set; }
+    public int TotalPage { get; set; }
     /// <summary>
     /// 总条数
     /// </summary>
-    public int TotalCount { get; private set; }
+    public int TotalCount { get; set; }
     /// <summary>
     /// 是否有上一页
     /// </summary>
